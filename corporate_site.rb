eeeb200505ab8059976dc8fa2c2eaa6b36fc6b9a
Lex <lexmasta@gmail.com>
2016-05-10 16:03:05 -0400

    pony express

--- corporate_site.rb
+++ corporate_site.rb
@@ -1,7 +1,28 @@
 require 'sinatra'
 
 get '/' do
-  File.read(File.join('index.html'))
+  File.read(File.join('index.html.erb'))
   # File.read(File.join('layout.html'))
   # File.read(File.join('reference.html'))
+end
+
+post '/' do
+    require 'pony'
+    Pony.mail(
+      from: "MIKE<mike@fusionrl.co>",
+      to: 'mike@fusionrl.co',
+      subject: "A message from the DAZ4126 website",
+      body: params[:name],params[:email],params[:message],
+      port: '587',
+      via: :smtp,
+      via_options: { 
+        :address              => 'smtp.sendgrid.net', 
+        :port                 => '587', 
+        :enable_starttls_auto => true, 
+        :user_name            => ENV['SENDGRID_USERNAME'], 
+        :password             => ENV['SENDGRID_PASSWORD'], 
+        :authentication       => :plain, 
+        :domain               => 'heroku.com'
+      })
+    redirect '/' 
 end
