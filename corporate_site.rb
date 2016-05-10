91f2dd8ec615556a73d7b1e6bb02909b86640902
Lex <lexmasta@gmail.com>
2016-05-10 18:42:05 -0400

    Removed error loggin attempts

--- corporate_site.rb
+++ corporate_site.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'better_errors'
-require 'logger'
 require 'pony'
 
 configure :development do
@@ -10,38 +9,26 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-class SinatraLoggingExample < Sinatra::Base
- 
-  ::Logger.class_eval { alias :write :'<<' }
-  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'..','log','access.log')
-  access_logger = ::Logger.new(access_log)
-  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'..','log','error.log'),"a+")
-  error_logger.sync = true
- 
-  configure do
-    use ::Rack::CommonLogger, access_logger
-  end
-
-	get '/?' do
-	  File.read(File.join('index.html'))
-	end
-
-	post '/mail/' do
-
-		# Pony.options = {   
-	 #                   :from           => "MIKE<mike@fusionrl.co>",
-	 #                   :via            => :smtp,
-	 #                   :via_options    => {
-	 #                     :address        => 'smtp.sendgrid.net',
-	 #                     :port           => '587',
-	 #                     :user_name      => ENV['SENDGRID_USERNAME'],
-	 #                     :password       => ENV['SENDGRID_PASSWORD'],
-	 #                     :authentication => :plain, 
-	 #                     :domain         => 'heroku.com'
-	 #                    }
-	 #                 }
-	 #  Pony.mail(subject: "A message from the #{params[:name]}", to: 'mike@fusionrl.co', body: "#{params[:name]} #{params[:email]} #{params[:message]}")
-		redirect('/?')
-	end
-
+get '/?' do
+  File.read(File.join('index.html'))
 end
+
+post '/mail' do
+
+	# Pony.options = {   
+ #                   :from           => "MIKE<mike@fusionrl.co>",
+ #                   :via            => :smtp,
+ #                   :via_options    => {
+ #                     :address        => 'smtp.sendgrid.net',
+ #                     :port           => '587',
+ #                     :user_name      => ENV['SENDGRID_USERNAME'],
+ #                     :password       => ENV['SENDGRID_PASSWORD'],
+ #                     :authentication => :plain, 
+ #                     :domain         => 'heroku.com'
+ #                    }
+ #                 }
+ #  Pony.mail(subject: "A message from the #{params[:name]}", to: 'mike@fusionrl.co', body: "#{params[:name]} #{params[:email]} #{params[:message]}")
+	redirect('/?')
+end
+
+
